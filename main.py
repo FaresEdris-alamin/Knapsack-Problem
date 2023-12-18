f38999bdd2bdf9a228721b62f01182df4425913a
Ahmed <NateRiver32000@gmail.com>
2023-12-18 21:53:30 +0200

    Experimental file

--- main.py
+++ main.py
@@ -31,16 +31,29 @@ class GeneticKnapsackSolver:
         fitness_values = [self.calculate_fitness(chromosome) for chromosome in self.population]
         fitness_values = [float(i) / sum(fitness_values) for i in fitness_values]
 
-        parent1 = random.choices(self.population, weights=fitness_values, k=1)[0]
-        parent2 = random.choices(self.population, weights=fitness_values, k=1)[0]
+        #choose 20% of the population as parents
+        sorted_population = [x for _, x in sorted(zip(fitness_values, self.population), reverse=True)]
+        parents = sorted_population[:int(0.2 * len(self.population))]
 
-        return parent1, parent2
+        return parents
 
-    def crossover(self, parent1, parent2):
+    def crossover(self, parents):
         crossover_point = random.randint(0, len(self.items) - 1)
-        child1 = parent1[0:crossover_point] + parent2[crossover_point:]
-        child2 = parent2[0:crossover_point] + parent1[crossover_point:]
-        return child1, child2
+
+        # Ensure there are at least two parents
+        if len(parents) < 2:
+            raise ValueError("At least two parents are required for crossover.")
+
+        # Perform crossover between all pairs of parents
+        children = []
+        for i in range(0, len(parents), 2):
+            parent1, parent2 = parents[i], parents[i + 1] if i + 1 < len(parents) else parents[i]
+            child1 = parent1[:crossover_point] + parent2[crossover_point:]
+            child2 = parent2[:crossover_point] + parent1[crossover_point:]
+            children.extend([child1, child2])
+
+        return children
+
 
     def mutate(self, chromosome):
         mutate_point = random.randint(0, len(self.items) - 1)
